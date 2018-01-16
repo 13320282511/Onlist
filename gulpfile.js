57a4b091e7bb2be80c68c2d2c1ba9de773e75dd4
zhaojian <745984265@qq.com>
2018-01-16 11:15:28 +0800

    sixday

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('testAutoFx', function () {
-    gulp.src('css/index.css')
+    gulp.src('src/index.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions', 'Android >= 4.0'],
             cascade: true, //是否美化属性值 默认：true 像这样：
@@ -10,5 +10,5 @@ gulp.task('testAutoFx', function () {
             //        transform: rotate(45deg);
             remove:true //是否去掉不必要的前缀 默认：true
         }))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('css/'));
 });
